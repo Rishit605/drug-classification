ced8f71cef96db93a3dfe68e5851ccd91a831c08
Rishit Sisodia <rishit.sisodia2001@gmail.com>
2022-07-26 22:09:51 +0530

    "'post' and 'get' request for message and data stream"

--- chat-app/target/debug/deps/chat_app.d
+++ chat-app/target/debug/deps/chat_app.d
@@ -3,3 +3,5 @@ C:\Users\pc\Desktop\Code\Git\New folder\git\chat-app\target\debug\deps\chat_app.
 C:\Users\pc\Desktop\Code\Git\New folder\git\chat-app\target\debug\deps\chat_app.d: src\main.rs
 
 src\main.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=C:\\Users\\pc\\Desktop\\Code\\Git\\New folder\\git\\chat-app
